159f78788c42d8faaf3bc2d9cfa52ba83529c18b
dcima <dcima@hotmail.com>
2021-02-22 10:41:29 +0100

    reato metodo updateShouldNotify

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -9,4 +9,10 @@ class Provider extends InheritedWidget {
     // ignore: deprecated_member_use
     return (context.inheritFromWidgetOfExactType(Provider) as Provider).bloc;
   }
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+    throw UnimplementedError();
+  }
 }
